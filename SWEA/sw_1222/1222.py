81229a9d77315b445a47ad5f973bfcfa14039e66
대전 7반 이민상 <alstkd2919@naver.com>
2024-07-23 16:16:40 +0900

    문제만을 위한 코드

---
+++ SWEA/sw_1222/1222.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+def postfix_natation_plus(string_num, arr):
+    result = []
+    for i in range(len(arr)):
+        if arr[i] >= '0' and arr[i] <= '9':
+            result.append(arr[i])
+        elif arr[i] == '+' and len(result) > 1:
+            num1 = int(result.pop())
+            num2 = int(result.pop())
+            result.append(num1 + num2)
+    return result[0] + int(arr[len(arr)-1])
+
+for test_case in range(1, 11):
+    n = int(input())
+    input_str = list(input())
+    print(f'#{test_case} {postfix_natation_plus(n, input_str)}')
