93fce1b6890f6ce85ecbd52461c4f23b517a27a1
minsanglee <alstkd2919@naver.com>
2024-10-12 23:10:55 +0900

    Pro 문자열 잘라서 정렬하기

--- Programmers/코딩기초트레이닝/문자열 잘라서 정렬하기.py
+++ Programmers/코딩기초트레이닝/문자열 잘라서 정렬하기.py
@@ -1,3 +1,8 @@
-from ctypes import pythonapi
-
-pythonapi
+def solution(myString):
+    result = myString.split("x")
+    result.sort()
+    real_result = []
+    for idx, ch in enumerate(result):
+        if ch != "":
+            real_result.append(ch)
+    return real_result
