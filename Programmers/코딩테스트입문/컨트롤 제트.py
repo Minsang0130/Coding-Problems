f97344fd13d1adc05456faa6851dbfe9afed1368
minsang lee <alstkd2919@naver.com>
2025-02-16 22:42:05 +0900

    컨트롤 제트

--- Programmers/코딩테스트입문/컨트롤 제트.py
+++ Programmers/코딩테스트입문/컨트롤 제트.py
@@ -1,4 +1,9 @@
-def solution(n) {
-  n = 0
-  return answer
-}
+def solution(s):
+    result = 0
+    s = s.split()
+    for idx, i in enumerate(s):
+        if i == "Z":
+            result -= int(s[idx-1])
+        else:
+            result += int(i)
+    return result
