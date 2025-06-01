64a6725b7caa7b403386850636c608d1a7a1a100
minsang lee <alstkd2919@naver.com>
2025-06-01 23:14:27 +0900

    Pro : Python 개발자 찾기

--- Baekjoon/2841.py
+++ Baekjoon/2841.py
@@ -1,10 +1,20 @@
 import sys
-from collections import deque
 
 def main():
-    N, P = sys.stdin.readline().split()
-    guitar_dict = dict()
+    N, P = map(int, sys.stdin.readline().split())
+    guitar_list = [[] * P for _ in range(7)]
+    count = 0
 
+    for _ in range(N):
+        n, p = map(int, sys.stdin.readline().split())
+        while guitar_list[n] and guitar_list[n][-1] > p:
+            guitar_list[n].pop()
+            count += 1
+        if not guitar_list[n] or guitar_list[n][-1] < p:
+            guitar_list[n].append(p)
+            count += 1
+
+    print(count)
 
 if __name__ == "__main__":
     main()
