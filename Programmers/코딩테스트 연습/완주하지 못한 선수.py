baa88a5681d925ff787ea9c71a00b72c91ab8b05
minsanglee <alstkd2919@naver.com>
2025-08-06 22:53:37 +0900

    Pro : 완주하지 못한 선수

---
+++ Programmers/코딩테스트 연습/완주하지 못한 선수.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def solution(participant, completion):
+    
+    part_count = Counter(participant)
+    comp_count = Counter(completion)
+
+    diff = part_count - comp_count
+
+    return list(diff.keys())[0]
